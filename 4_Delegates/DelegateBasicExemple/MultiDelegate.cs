0a4d917034678d9678cc7c5c4c3ff2a933fd4798
Igor <lozovanu.igor.19@gmail.com>
2025-02-19 09:13:06 +0100

    Multicast delegate

---
+++ 4_Delegates/DelegateBasicExemple/MultiDelegate.cs
@@ -0,0 +1,40 @@
+﻿namespace Delegate
+{
+    class MultiDelegate
+    {
+        delegate void LogDel(string text);
+
+        public static void Execute()
+        {
+            // Multicast delegate
+            Log log = new Log();
+
+            LogDel LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+            LogDel LogTextToFileDel = new LogDel(log.LogTextToFile);
+            LogDel multilogDel = LogTextToScreenDel + LogTextToFileDel;
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            multilogDel(name);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Log
+    {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+}
