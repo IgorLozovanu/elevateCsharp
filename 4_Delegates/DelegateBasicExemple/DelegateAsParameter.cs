fda30e2bfda4bca6740cfbd586e6cc1556c4feee
Igor <lozovanu.igor.19@gmail.com>
2025-02-19 19:35:57 +0100

    Passing a delegate as parameter to a method

---
+++ 4_Delegates/DelegateBasicExemple/DelegateAsParameter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBasicExemple
+{
+    class DelegateAsParameter
+    {
+        delegate void LogDel(string text);
+
+        public static void Execute()
+        {
+            LogDel logTextToScreen = new LogDel(LogTextToScreen);
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            LogText(logTextToScreen, name);
+        }
+
+        static void LogText(LogDel logDel, string text)
+        {
+            logDel(text);
+        }
+
+        static void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+    }
+}
--- 4_Delegates/DelegateBasicExemple/Program.cs
+++ 4_Delegates/DelegateBasicExemple/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Delegate
+﻿using DelegateBasicExemple;
+
+namespace Delegate
 {
     class Program
     {
         static void Main(string[] args)
         {
             MultiDelegate.Execute();
-
+            DelegateAsParameter.Execute();
 
             Console.ReadKey();
         }
