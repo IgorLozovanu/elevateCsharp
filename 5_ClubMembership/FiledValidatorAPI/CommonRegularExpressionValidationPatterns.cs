337c5db8f3a1334dc42fb90c7ace4f2d1b15230b
Igor <lozovanu.igor.19@gmail.com>
2025-02-21 17:16:17 +0100

    Added regex patterns for fields check

---
+++ 5_ClubMembership/FiledValidatorAPI/CommonRegularExpressionValidationPatterns.cs
@@ -0,0 +1,13 @@
+﻿namespace FiledValidatorAPI
+{
+    public static class CommonRegularExpressionValidationPatterns
+    {
+        public const string Email_Address_RegEx_Pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        public const string Uk_PhoneNumber_RegEx_Pattern = @"^(\+44\s?|0)7\d{9}$";
+
+        public const string Uk_Post_Code_RegEx_Pattern = @"^([GIR]0AA|[A-Z]{1,2}\d[A-Z\d]? \d[A-Z]{2})$";
+
+        public const string Strong_Passwprd_RegEx_Pattern = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+    }
+}
